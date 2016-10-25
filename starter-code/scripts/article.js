41b27474610e4fa4e3d07fa1a2a8dd4be31212a7
sueanyv <valenciasue@hotmail.com>
2016-10-25 15:57:16 -0700

    stuff

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,18 +1,20 @@
 var articles = [];
 
-function Article (options) {
-/* TODO: This is our Model constructor. It will take in
-   our source data from blogArticles and instantiate a
-   new Object according to this new definition. options is
-   a placeholder for the object that will ultimately be
-   passed in. Use all of the properties in blogArticles
-   to populate the new Article data that we'll use.  */
+function Article(options) {
+  /* TODO: This is our Model constructor. It will take in
+     our source data from blogArticles and instantiate a
+     new Object according to this new definition. options is
+     a placeholder for the object that will ultimately be
+     passed in. Use all of the properties in blogArticles
+     to populate the new Article data that we'll use.  */
+  this.title = options.title;
 };
 
 Article.prototype.toHtml = function() {
-
+  var $newArticle = $('article.template').clone();
+  $newArticle.attr('data-category', this.category);
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-  $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000) + ' days ago');
 
   return $newArticle;
 };
