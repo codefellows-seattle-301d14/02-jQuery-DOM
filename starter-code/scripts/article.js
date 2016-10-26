b27050043eaaa686ef8f3c395bbd36c26ac34e06
Hawa Abdi <abdih17@hotmail.com>
2016-10-25 17:51:53 -0700

    Completed the TODO assignments.

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -25,10 +25,18 @@ Article.prototype.toHtml = function() {
   3. article title
   4. article body
   5. publication*/
+  // DONE!
+  $newArticle.find('a').text(this.author);
+  $newArticle.find('a').attr('href', this.authorUrl);
+  $newArticle.find('h1').text(this.title);
+  $newArticle.find('section').html(this.body);
+  $newArticle.find('time').text(this.publishedOn);
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
 /* TODO: This cloned article is no longer a template, as it now
 has real data attached to it. Remove the class from this new article! */
+// DONE!
+  $newArticle.removeClass('template');
   return $newArticle;
 };
 
